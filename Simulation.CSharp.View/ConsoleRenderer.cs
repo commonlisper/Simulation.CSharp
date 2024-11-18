b31fc8f125dedaa486471cda044f3299fd158bc1
Common Lisper <atosdo@live.com>
2024-11-18 16:55:02 +0200

    add View project with Renderer classes

---
+++ Simulation.CSharp.View/ConsoleRenderer.cs
@@ -0,0 +1,13 @@
+﻿using Simulation.CSharp.Domain.Field;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.CSharp.View
+{
+    public class ConsoleRenderer(Map map) : Renderer(map)
+    {
+    }
+}
---
+++ Simulation.CSharp.View/Renderer.cs
@@ -0,0 +1,9 @@
+﻿using Simulation.CSharp.Domain.Field;
+
+namespace Simulation.CSharp.View
+{
+    public abstract class Renderer(Map map)
+    {
+        private readonly Map _map = map;
+    }
+}
